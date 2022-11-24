e81723923c9de2721d000b7f5037957b264743cc
Carmen Shannon <99128567+Carmen-Shannon@users.noreply.github.com>
2022-11-23 23:45:41 -0500

    Update DemoApplication.java

--- src/main/java/com/forbesapi/demo/DemoApplication.java
+++ src/main/java/com/forbesapi/demo/DemoApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-	
-	// storing the story as a public static variable so it can be accessed by the appropriate controller
-    public static String story = "Forbes belives in the power of entreprenuerial capitalizm and uses it various platforms to ignight the conversations that drive systemic change in buziness, culture and society. We celebrate sucess and are comited to using our megaphone to drive equity. We are the worlds biggest business media brand and we consisstently place in the top 20 of the most-popular sites in the United States, in good company with brands lik Netflix, Apple and Google. In short, we have a bige platforme and we use it responsiblie.";
 
 	public static void main(String[] args) {
 		// Setting port to 8080 for our http server and running the application
